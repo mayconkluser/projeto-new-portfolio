5ae98132ba92e9c920c5cd425ed6eb4cb2e06bcb
Maycon Klüser <maycon10kluser@gmail.com>
2022-07-18 00:56:33 -0300

    Realizado função calcular a distância percorrida pelo mousemove no slide, nas cards do portfólio.

--- js/modulo/portfolio-slide.js
+++ js/modulo/portfolio-slide.js
@@ -2,26 +2,46 @@ export default class PortfolioSlide {
   constructor(divPortfolio, ulPortfolio) {
     this.divPortfolio = document.querySelector(divPortfolio);
     this.ulPortfolio = document.querySelector(ulPortfolio);
+    this.distancias = {
+      posicaoFinal: 0,
+      inicioX: 0,
+      totalMovimento: 0,
+    };
   }
 
-  ComecarEvento(event) {
+  moverEvento(distanciaX) {
+    this.distancias.posicaoDoMovimento = distanciaX;
+    this.divPortfolio.style.transform = `translate3d(${distanciaX}px,0,0)`;
+  }
+
+  atualizarPosicao(clientX) {
+    this.distancias.totalMovimento = (this.distancias.inicioX - clientX) * 1.6;
+    return this.distancias.posicaoFinal - this.distancias.totalMovimento;
+  }
+
+  comecarEvento(event) {
     event.preventDefault();
+    this.distancias.inicioX = event.clientX;
     this.ulPortfolio.addEventListener("mousemove", this.quandoMover);
   }
 
   terminarEvento(event) {
     this.ulPortfolio.removeEventListener("mousemove", this.quandoMover);
+    this.distancias.posicaoFinal = this.distancias.posicaoDoMovimento;
   }
 
-  quandoMover(event) {}
+  quandoMover(event) {
+    const posicaoFinal = this.atualizarPosicao(event.clientX);
+    this.moverEvento(posicaoFinal);
+  }
 
   adicionarEventoSlide() {
-    this.ulPortfolio.addEventListener("mousedown", this.ComecarEvento);
+    this.ulPortfolio.addEventListener("mousedown", this.comecarEvento);
     this.ulPortfolio.addEventListener("mouseup", this.terminarEvento);
   }
 
   eventosBind() {
-    this.ComecarEvento = this.ComecarEvento.bind(this);
+    this.comecarEvento = this.comecarEvento.bind(this);
     this.quandoMover = this.quandoMover.bind(this);
     this.terminarEvento = this.terminarEvento.bind(this);
   }
