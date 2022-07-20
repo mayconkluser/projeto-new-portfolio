9e63bbdaf66589bbd4cc67d390c903e12374c0db
Maycon Klüser <maycon10kluser@gmail.com>
2022-07-19 22:34:39 -0300

    calculo de distancias quando os eventos ocorrem

--- js/modulo/portfolio-slide.js
+++ js/modulo/portfolio-slide.js
@@ -2,21 +2,37 @@ export default class PortfolioSlide {
   constructor(slide, containerSlide) {
     this.slide = document.querySelector(slide);
     this.containerSlide = document.querySelector(containerSlide);
+    this.dist = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0,
+    };
+  }
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPosition - this.dist.movement;
   }
 
   onStart(event) {
     event.preventDefault();
-    this.containerSlide.addEventListener("mousemove", this.onStart);
-    console.log("mousemove");
+    this.dist.startX = event.clientX;
+    this.containerSlide.addEventListener("mousemove", this.onMove);
   }
 
   onMove(event) {
-    console.log("movendo");
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
 
   onEnd(event) {
-    this.containerSlide.removeEventListener("mousemove", this.onStart);
-    console.log("mouseup");
+    this.containerSlide.removeEventListener("mousemove", this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
   }
 
   addSlideEvents() {
